16b94c7eef10b0be46d97ac0faad1e89f06859b3
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-25 21:42:53 +0000

    Handle request errors and log error details to console

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,10 @@ const request  = require('request');
 
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv.slice(2).toString()}`
 request(url, (error, response, body) => {
-  if(error) throw error;
+  // console.log(url);
+  if(error){
+    console.log(error);
+  };
   console.log(`status code: `, response.statusCode);
   const data = JSON.parse(body);
   console.log(data[0].description)
