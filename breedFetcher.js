f7220f240541fd7307445bf520e9d31dcc9f6029
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-25 19:08:31 +0000

    Fetch and deserialize api data on Node

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,8 @@
-const { request } = require('request');
+const request  = require('request');
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=sib`;
+request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, response, body) => {
+  if(error) throw error;
+  console.log(`status code: `, response.statusCode);
+  const data = JSON.parse(body);
+})
