f6d441ac735b311bbf016301214fe41c75c4dcf1
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-25 22:49:19 +0000

    Handle edge cases: failed request || breed name not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,17 @@
-const request  = require('request');
+const request = require("request");
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv.slice(2).toString()}`
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv
+  .slice(2)
+  .toString()}`;
 request(url, (error, response, body) => {
-  // console.log(url);
-  if(error){
-    console.log(error);
-  };
-  console.log(`status code: `, response.statusCode);
+  if (error) throw error;
   const data = JSON.parse(body);
-  console.log(data[0].description)
-})
+  console.log("test: ", data.length);
+  if (data.length === 0) {
+    console.log("Breed name not found!");
+    return;
+  }
+  console.log(data[0].description);
+  let name = data[0].name;
+  console.log("name", name);
+});
