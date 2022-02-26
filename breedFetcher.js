d9f3dd4e3cb09714facd7cf79ad947dc0db4ff5a
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-26 10:41:02 +0000

    Refactor fxn to accept breedName variable as per test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
-const fetchBreedDescription = function(url, callback) {
-  request(url, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
       return callback(error, null);
     }
