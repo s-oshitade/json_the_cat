98072daa745e518a3c6e2b1d1616c76dba1554a8
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-26 11:15:23 +0000

    Refactor fxn to pass test with invalid breed names

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request = require("request");
 
-const fetchBreedDescription = function(breedName, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      return callback(error, null);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        return callback(error, null);
+      }
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null, "Breed not found!");
+      } else {
+        callback(null, data[0].description);
+      }
     }
-    const data = JSON.parse(body);
-    let description = data[0].description;
-    if (data.length === 0) {
-      callback("Breed not found!");
-    } else {
-      callback(null, description);
-    }
-  });
+  );
 };
 
 module.exports = { fetchBreedDescription };
